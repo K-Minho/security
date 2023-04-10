389a7981dfea9c722009eafe3c9758fdae9e0ca1
kkr <kkr0787@gmail.com>
2023-04-10 14:19:41 +0900

    responseDTO

--- src/main/java/xyz/cyanin/securityapp/service/UserService.java
+++ src/main/java/xyz/cyanin/securityapp/service/UserService.java
@@ -17,6 +17,12 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
+    // 1. 트랜젝션 관리
+    // 2. 영속성 객체 변경감지
+    // 3. RequestDTO 요청받기
+    // 4. 비즈니스 로직 처리
+    // 5. ResponseDTO 응답하기
+
     @Transactional
     public User 회원가입(UserRequest.JoinDTO joinDTO){ // 내부적인 프로세스 통신이면 한글로 써도 문제 없음
         String rawPassword = joinDTO.getPassword();
