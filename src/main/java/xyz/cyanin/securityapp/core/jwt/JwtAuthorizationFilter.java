e1057243c49724b84ecf334543523ac1b97cb40a
K-Minho <alsgh9979@naver.com>
2023-04-11 16:24:33 +0900

    jwt filter rewrite

--- src/main/java/xyz/cyanin/securityapp/core/jwt/JwtAuthorizationFilter.java
+++ src/main/java/xyz/cyanin/securityapp/core/jwt/JwtAuthorizationFilter.java
@@ -7,7 +7,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -17,55 +16,41 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import xyz.cyanin.securityapp.core.auth.MyUserDetails;
-import xyz.cyanin.securityapp.dto.ResponseDTO;
 import xyz.cyanin.securityapp.model.User;
 
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
-    private AuthenticationManager authenticationManager;
-
     public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
-        this.authenticationManager = authenticationManager;
     }
-
-    private void error(HttpServletResponse resp, Exception e) throws IOException {
-        resp.setStatus(401);
-        resp.setContentType("application/json; charset=utf-8");
-        ResponseDTO<?> responseDto = new ResponseDTO<>().fail(500, "인증 안됨", e.getMessage());
-        ObjectMapper om = new ObjectMapper();
-        String responseBody = om.writeValueAsString(responseDto);
-        resp.getWriter().println(responseBody);
-    }
-
+    
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         String prefixJwt = request.getHeader(MyJwtProvider.HEADER);
-        if(prefixJwt == null){
-            error(response, new RuntimeException("토큰이 전달되지 않았습니다"));
-            return;
-        }
-        String jwt = prefixJwt.replace(MyJwtProvider.TOKEN_PREFIX, "");
-        try {
-            DecodedJWT decodedJWT = MyJwtProvider.verify(jwt);
-            Long id = decodedJWT.getClaim("id").asLong();
-            String role = decodedJWT.getClaim("role").asString();
+        if(prefixJwt != null){
+            String jwt = prefixJwt.replace(MyJwtProvider.TOKEN_PREFIX, "");
+            try {
+                DecodedJWT decodedJWT = MyJwtProvider.verify(jwt);
+                Long id = decodedJWT.getClaim("id").asLong();
+                String role = decodedJWT.getClaim("role").asString();
 
-            User user = User.builder().id(id).role(role).build();
-            
-            MyUserDetails myUserDetails = new MyUserDetails(user);
-            Authentication authentication2 = new UsernamePasswordAuthenticationToken(myUserDetails, myUserDetails.getPassword(), myUserDetails.getAuthorities());
-            
-            SecurityContextHolder.getContext().setAuthentication(authentication2);
-        
-            chain.doFilter(request, response);
-        }catch (SignatureVerificationException sve){
-            error(response, sve);
-        }catch (TokenExpiredException tee){
-            error(response, tee);
+                User user = User.builder().id(id).role(role).build();
+                MyUserDetails myUserDetails = new MyUserDetails(user);
+                Authentication authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                myUserDetails,
+                                myUserDetails.getPassword(),
+                                myUserDetails.getAuthorities()
+                        );
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }catch (SignatureVerificationException sve){
+                chain.doFilter(request, response);
+            }catch (TokenExpiredException tee){
+                chain.doFilter(request, response);
+            }
         }
+        chain.doFilter(request, response);
     }
 }
